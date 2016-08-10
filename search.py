82e833d7818986079643af159367d0825b4d56be
igor.pochitalkin <igor.pochitalkin@everys.com>
2016-08-10 17:56:33 +0300

    search finally works correct in search.py file

--- search.py
+++ search.py
@@ -2,10 +2,10 @@ __author__ = 'igor.pochitalkin'
 import os, zipfile, math, time
 
 arch_name = "db_install_" + time.strftime("%Y%m%d" "_" "%H%M")  #backup archive name
-file = "WPHARMA.EXE"                                            #search for a winpharma directory
-copie = "BACKUPDB.BIN"                                          #search for a backup directory
+file = "WPHARMA.EXE"                                            #winpharma directory
+copie = "BACKUPDB.BIN"                                          #backup directory
 
-def find_dir(name, path):
+def find_dir(name, path):                                       #searching directory
     result = None                                               #directory
     timer = 0                                                   #directory choosing criteria(by usage time)
     for root, dirs, files in os.walk(path):
@@ -14,11 +14,19 @@ def find_dir(name, path):
             if timer < lastcall:
                 timer = lastcall
                 result = os.path.normpath(root)                 #path to directory containing file
+                #print(lastcall, result)
     return  result
 
 to_save = (find_dir(file, "C:/") + "\DB")                       #finding last used wpharma.exe file
-print(to_save)
+#print(to_save)
 
 saving_place = (find_dir(copie, "C:/"))                         #finding last used copie_db folder
-print(saving_place)
+#print(saving_place)
 
+arch_fullpath = saving_place + "\\" + arch_name + ".zip"
+#print(arch_fullpath)
+
+arch = zipfile.ZipFile(arch_fullpath, "w")
+for files in os.listdir(to_save):
+    arch.write(os.path.join(to_save, files))
+arch.close()
